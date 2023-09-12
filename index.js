308ab67465fd2cb4e376f8410fbc186f445f476e
Margaret Pope <mmp298@gmail.com>
2023-09-11 21:51:59 -0500

    first attempt

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
-// Your code here
+var questionsArr = [
+    {
+        question: "Birmingham is in Alabama.",
+        answer: true
+    },
+    {
+        question: "Birmingham is the capital of Alabama.",
+        answer: false
+    },
+    {
+        question: "Birmingham was founded in 1871.",
+        answer: true
+    },
+    {   question: "Martin Luther King, Jr. never came to Birmingham.",
+        answer: false
+    },
+    {   question: "Michael Jordan played for the Birmingham Barons.",
+        answer: true
+    },    
+];
+function runQuiz() {
+    var correct = questionsArr.answer
+    for (var i = 0; i < questionsArr.length; i++) {
+        var quizQuestion = questionsArr[i]
+        var quizAnswer = confirm(quizQuestion)
+        if (quizAnswer) {
+            correct++
+        }
+    }   
+}
